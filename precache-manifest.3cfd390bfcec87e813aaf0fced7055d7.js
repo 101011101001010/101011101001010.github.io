886ddafd622cc58774709c99e8d0342c8ab2ed73
Teo Wei Jie <wjayteo@gmail.com>
2019-08-30 01:00:13 +0800

    Updates

---
+++ precache-manifest.3cfd390bfcec87e813aaf0fced7055d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87bf7f2c14d03c2fc5776dde8b57b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf8ff479314e8108618",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "545488a46a22f76eb85b",
+    "url": "/static/js/2.9b34b5d3.chunk.js"
+  },
+  {
+    "revision": "faf8ff479314e8108618",
+    "url": "/static/js/main.2781da64.chunk.js"
+  },
+  {
+    "revision": "097581bd33d4e793723c",
+    "url": "/static/js/runtime~main.0027a31a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
